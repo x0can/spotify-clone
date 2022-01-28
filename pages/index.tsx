a70f968776b59477d4b072b7abe4f3c63a2edc39
alexmwaura <alexmwaura43@gmail.com>
2022-01-28 09:44:21 +0300

    profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import GradientLayout from "../components/gradientLayout";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <GradientLayout
+      roundImage
+      color="teal"
+      title="Alex Mwaura"
+      subtitle="profile"
+      description="15 public playlist"
+      image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
+    >
+      <div>Home</div>;
+    </GradientLayout>
+  );
 };
 
 export default Home;
